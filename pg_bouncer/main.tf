8fd4aef3f5ff43d4397a3674d65c6c9a32b7e239
nischit <nischitpra@gmail.com>
2025-01-17 21:46:54 +0545

    terraform provider

--- pg_bouncer/main.tf
+++ pg_bouncer/main.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_version = "~> 1.1.0"
+
+  required_providers {
+     kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.20"
+    }
+  }
+}
+
 provider "kubernetes" {
   config_path = "~/.kube/config" # Update if using a specific KUBECONFIG
 }
